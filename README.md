622afb38749b9fba181de2021e40bb90a649ca81
talebSleiman <talebsleiman4@gmail.com>
2023-03-06 23:19:30 +0200

    Challenge Complete

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Intro-Section-With-Dropdown-Navigation
-This challenge will test your ability to create dropdown navigation menus, a common pattern on larger sites. It will also provide some nice basic layout challenges.
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Links
+
+- Solution URL:  ()
+- Live Site URL: ()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript
+
+## Author
+
+- Frontend Mentor - [@talebSleiman](https://www.frontendmentor.io/profile/talebSleiman)
+
---
+++ main.js
@@ -0,0 +1,42 @@
+let feautures = document.querySelector(".features");
+let company = document.querySelector(".company");
+
+let feauturesImg = document.querySelector(".feautures-img");
+let companyImg = document.querySelector(".company-img");
+
+let featuresBlog = document.querySelector(".features-blog");
+let companyBlog = document.querySelector(".company-blog");
+
+let closeMenu = document.querySelector(".icon-close-menu");
+let openMenu = document.querySelector(".icon-menu");
+let nav = document.querySelector("nav");
+
+openMenu.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+    nav.classList.remove("active");
+});
+
+
+feautures.addEventListener("click", () => {
+    changeArrow(feauturesImg);
+    featuresBlog.classList.toggle("display-none");
+});
+
+company.addEventListener("click", () => {
+    changeArrow(companyImg);
+    companyBlog.classList.toggle("display-none");
+});
+
+
+function changeArrow(element) {
+    if (element.classList.contains("down")) {
+        element.classList.toggle("down");
+        element.setAttribute("src", "./images/icon-arrow-up.svg");
+    } else {
+        element.classList.toggle("down");
+        element.setAttribute("src", "./images/icon-arrow-down.svg");
+    }
+}
